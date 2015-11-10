c612aeb43877ae45ce4a6a5782bb2b24d17cf64e
zenond <david.zenon@hotmail.fr>
2015-11-10 12:24:07 +0100

    RechercheTest et Recherche --> Ajout Code

--- src/main/java/Recherche.java
+++ src/main/java/Recherche.java
@@ -1,6 +1,54 @@
+import static org.junit.Assert.assertTrue;
 
-public class Recherche {
+import java.util.ArrayList;
+
+public class Recherche<T>{
 	
-	 int index;
+	private ArrayList<Comparable<T>> tableau;
+	
+	public Recherche(ArrayList<T> tab)
+	{
+		tableau = (ArrayList<Comparable<T>>) tab;
+	}
+	
+	public int RechercheValeur(T Objet)
+	{
+		int taille = tableau.size();
+		Boolean sortie = false;
+		int i = ((taille-1)/2);	
+		do
+		{	
+			System.out.println("i = " + i);
+			System.out.println("tableau.get(i) : " + tableau.get(i));
+			System.out.println("tableau.get(i).compareTo(Objet) : " + tableau.get(i).compareTo(Objet));
+			System.out.println("sortie = " + sortie);
+
+			if ((tableau.get(i).compareTo(Objet)) > 0){
+				i = (i/2);
+			}
+			else if ((tableau.get(i).compareTo(Objet)) == 0)
+				sortie = true;
+			else
+				i = i + (i/2);
+		}while((sortie == false) || (i >= taille) || (i<0));
+		
+		System.out.println("sortie = " + sortie);
+
+		return i;
+	}
+	/*
+	public static void main(String [] args)
+	{
+		ArrayList<Integer> intTab = new ArrayList<Integer>();
+		intTab.add(6);
+		intTab.add(8);
+		intTab.add(10);
+		intTab.add(12);
+		intTab.add(14);
+		
+		Recherche tabInt = new Recherche(intTab);
+		
+		assertTrue(tabInt.RechercheValeur(6) == 0);
+	}*/
 
 }
--- src/test/java/RechercheTest.java
+++ src/test/java/RechercheTest.java
@@ -1,8 +1,30 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Assert;
+import org.junit.Test;
 
 public class RechercheTest {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	@Test
+	public void TestInteger()
+	{
+		ArrayList<Integer> intTab = new ArrayList<Integer>();
+		intTab.add(6);
+		intTab.add(8);
+		intTab.add(10);
+		intTab.add(12);
+		intTab.add(14);
+		
+		Recherche tabInt = new Recherche(intTab);
+		
+		System.out.println("Valeur : 6");
+		assertTrue(tabInt.RechercheValeur(6) == 0);
+		System.out.println("Valeur : 12");
+		assertTrue(tabInt.RechercheValeur(12) == 3);
+		System.out.println("Valeur : 14");
+		assertTrue(tabInt.RechercheValeur(14) == 4);
 
 	}
 
