3422003dca0b2263b08eebbbb932c4aef2c07431
zenond <david.zenon@hotmail.fr>
2015-11-14 01:42:07 +0100

    Mise à jour du code RechercheValeur.
    ChangeLog : 
    
    Marche pour tableau statique et
    dynamique de type int, float, String.
    
    Prochainement, faire marcher le code
    avec des classes Objets qu'on crée
    nous même.

--- src/main/java/Recherche.java
+++ src/main/java/Recherche.java
@@ -1,54 +1,77 @@
-import static org.junit.Assert.assertTrue;
-
 import java.util.ArrayList;
 
 public class Recherche<T>{
 	
 	private ArrayList<Comparable<T>> tableau;
 	
+	// Initialisation d'un tableau dynamique
+	@SuppressWarnings("unchecked")
 	public Recherche(ArrayList<T> tab)
 	{
-		tableau = (ArrayList<Comparable<T>>) tab;
+		tableau = new ArrayList<Comparable<T>>();
+		for (int i = 0; i < tab.size(); i++)
+			tableau.add((Comparable<T>) tab.get(i));		
 	}
 	
+	// Initialisation d'un tableau statique
+	public Recherche(Comparable<T> [] tab)
+	{
+		tableau = new ArrayList<Comparable<T>>();
+		for (int i = 0; i < tab.length; i++)
+			tableau.add(tab[i]);		
+	}
+	
+	// Algorithme de recherche par dichotomie
 	public int RechercheValeur(T Objet)
 	{
-		int taille = tableau.size();
+		int tailleMax = tableau.size()-1;
+		int tailleMin = 0;
 		Boolean sortie = false;
-		int i = ((taille-1)/2);	
+		int i;	
 		do
-		{	
+		{			
+			i = (tailleMax + tailleMin)/2;
+			
+		/*	System.out.println("**************************************************");
 			System.out.println("i = " + i);
 			System.out.println("tableau.get(i) : " + tableau.get(i));
 			System.out.println("tableau.get(i).compareTo(Objet) : " + tableau.get(i).compareTo(Objet));
 			System.out.println("sortie = " + sortie);
+			System.out.println("**************************************************"); */
 
+			// Valeur souhaitée plus petit que la valeur du tableau 
 			if ((tableau.get(i).compareTo(Objet)) > 0){
-				i = (i/2);
+				tailleMax = i;
 			}
+			// Valeur souhaitée égale à la valeur du tableau
 			else if ((tableau.get(i).compareTo(Objet)) == 0)
 				sortie = true;
+			// Valeur souhaitée plus grand que la valeur du tableau
 			else
-				i = i + (i/2);
-		}while((sortie == false) || (i >= taille) || (i<0));
+				tailleMin = i;
+			
+			///////////////////////////////////////////////
+			
+			if ((tailleMax - tailleMin) <= 1)
+			{
+				if (tableau.get(tailleMax).compareTo(Objet) == 0)
+					i = tailleMax;
+				else if (tableau.get(tailleMin).compareTo(Objet) == 0)
+					i = tailleMin;
+				else
+					i = -1;
+			
+				sortie = true;
+			}
+			
+		}while(sortie == false);
 		
+		
+	/*	
 		System.out.println("sortie = " + sortie);
-
+		System.out.println("i final = " + i); */
+		
 		return i;
 	}
-	/*
-	public static void main(String [] args)
-	{
-		ArrayList<Integer> intTab = new ArrayList<Integer>();
-		intTab.add(6);
-		intTab.add(8);
-		intTab.add(10);
-		intTab.add(12);
-		intTab.add(14);
-		
-		Recherche tabInt = new Recherche(intTab);
-		
-		assertTrue(tabInt.RechercheValeur(6) == 0);
-	}*/
 
 }
--- src/test/java/RechercheTest.java
+++ src/test/java/RechercheTest.java
@@ -1,8 +1,5 @@
 import static org.junit.Assert.*;
-
-import java.util.ArrayList;
-
-import org.junit.Assert;
+import java.util.ArrayList; 
 import org.junit.Test;
 
 public class RechercheTest {
@@ -10,22 +7,109 @@ public class RechercheTest {
 	@Test
 	public void TestInteger()
 	{
+		ArrayList<Float> floatTab = new ArrayList<Float>();
+		floatTab.add((float) 6.0);
+		floatTab.add((float) 8.4);
+		floatTab.add((float) 10.1);
+		floatTab.add((float) 11.9);
+		floatTab.add((float) 12.95);
+		floatTab.add((float) 14.35);
+		floatTab.add((float) 15.49);
+		
+		Recherche<Float> floatRecherche = new Recherche<Float>(floatTab);
+		
+		System.out.println("Valeur : 6.0");
+		assertTrue(floatRecherche.RechercheValeur((float) 6.0) == 0);
+		System.out.println("Valeur : 12.0");
+		assertTrue(floatRecherche.RechercheValeur((float) 12.95) == 4);
+		System.out.println("Valeur : 14.35");
+		assertTrue(floatRecherche.RechercheValeur((float) 14.35) == 5);
+		System.out.println("Valeur : 9.54");
+		assertTrue(floatRecherche.RechercheValeur((float) 9.54) == -1);
+		System.out.println("Valeur : 15.49");
+		assertTrue(floatRecherche.RechercheValeur((float) 15.49) == 6);
+		
+		Float [] floatTabStatic = {(float) 6.0,(float) 7.9,(float) 8.5,(float) 15.36,(float) 19.5};
+		
+		floatRecherche = new Recherche<Float>(floatTabStatic);
+		
+		System.out.println("Valeur : 6.0");
+		assertTrue(floatRecherche.RechercheValeur((float) 6.0) == 0);
+		System.out.println("Valeur : 8.5");
+		assertTrue(floatRecherche.RechercheValeur((float) 8.5) == 2);
+		System.out.println("Valeur : 19.5");
+		assertTrue(floatRecherche.RechercheValeur((float) 19.5) == 4);
+		System.out.println("Valeur : 9.3");
+		assertTrue(floatRecherche.RechercheValeur((float) 9.3) == -1);
+		
+		//////////////////////////////////////////////////
+		
 		ArrayList<Integer> intTab = new ArrayList<Integer>();
 		intTab.add(6);
 		intTab.add(8);
 		intTab.add(10);
+		intTab.add(11);
 		intTab.add(12);
 		intTab.add(14);
+		intTab.add(15);
 		
-		Recherche tabInt = new Recherche(intTab);
+		Recherche<Integer> intRecherche = new Recherche<Integer>(intTab);
+
+		System.out.println("Valeur : 6");
+		assertTrue(intRecherche.RechercheValeur(6) == 0);
+		System.out.println("Valeur : 11");
+		assertTrue(intRecherche.RechercheValeur(11) == 3);
+		System.out.println("Valeur : 15");
+		assertTrue(intRecherche.RechercheValeur(15) == 6);
+		System.out.println("Valeur : 13");
+		assertTrue(intRecherche.RechercheValeur(13) == -1);
+		
+		Integer [] intTabStatic = {6,8,11,15,18};
+		intRecherche = new Recherche<Integer>(intTabStatic);
 		
 		System.out.println("Valeur : 6");
-		assertTrue(tabInt.RechercheValeur(6) == 0);
-		System.out.println("Valeur : 12");
-		assertTrue(tabInt.RechercheValeur(12) == 3);
+		assertTrue(intRecherche.RechercheValeur(6) == 0);
+		System.out.println("Valeur : 11");
+		assertTrue(intRecherche.RechercheValeur(11) == 2);
+		System.out.println("Valeur : 18");
+		assertTrue(intRecherche.RechercheValeur(18) == 4);
 		System.out.println("Valeur : 14");
-		assertTrue(tabInt.RechercheValeur(14) == 4);
+		assertTrue(intRecherche.RechercheValeur(14) == -1);
 
+		///////////////////////////////////////////////////
+		
+		ArrayList<String> stringTab = new ArrayList<String>();
+		stringTab.add("a");
+		stringTab.add("abc");
+		stringTab.add("cad");
+		stringTab.add("dee");
+		stringTab.add("def");
+		stringTab.add("fas");
+		stringTab.add("gga");
+		
+		Recherche<String> stringRecherche = new Recherche<String>(stringTab);
+
+		System.out.println("Valeur : a");
+		assertTrue(stringRecherche.RechercheValeur("a") == 0);
+		System.out.println("Valeur : cad");
+		assertTrue(stringRecherche.RechercheValeur("cad") == 2);
+		System.out.println("Valeur : dee");
+		assertTrue(stringRecherche.RechercheValeur("dee") == 3);
+		System.out.println("Valeur : gga");
+		assertTrue(stringRecherche.RechercheValeur("gga") == 6);
+		System.out.println("Valeur : z");
+		assertTrue(stringRecherche.RechercheValeur("z") == -1);
+		
+		String [] stringTabStatic = {"abc","def","ghi"};
+		stringRecherche = new Recherche<String>(stringTabStatic);
+		
+		System.out.println("Valeur : abc");
+		assertTrue(stringRecherche.RechercheValeur("abc") == 0);
+		System.out.println("Valeur : ghi");
+		assertTrue(stringRecherche.RechercheValeur("ghi") == 2);
+		System.out.println("Valeur : dgt");
+		assertTrue(stringRecherche.RechercheValeur("dgt") == -1);
+		
 	}
 
 }
