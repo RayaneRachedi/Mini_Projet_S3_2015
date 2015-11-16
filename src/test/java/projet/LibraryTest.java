bf08d68961b8e89922081021a5e14435cb89005b
Gitixel <TPCA6991@hotmail.fr>
2015-11-16 23:39:00 +0100

    Préparation de la branche maître à être merger avec les 2 sous-branches     - (re)création de la structure (package) du projet     - suppression des fichiers d'example     - préparation de la classe de Test globale.

---
+++ src/test/java/projet/LibraryTest.java
@@ -0,0 +1,19 @@
+package projet;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/*
+ * This Java source file was generated with gradle
+ * by 'Latixel' at '06/11/15 22:06' with Gradle 2.8
+ *
+ * @author Latixel, @date 06/11/15 22:06
+ */
+/**
+ * Classe de Test JUnit de la librairie (tri & recherche)
+ * @author Latixel
+ *
+ */
+public class LibraryTest {
+    // aucun test car effectué avec JUnit
+}
